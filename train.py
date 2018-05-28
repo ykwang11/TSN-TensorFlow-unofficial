da70e13444cf9a6152d65618a140dbb8b7efa730
Yu-Kai Wang <r03921060@ntu.edu.tw>
2018-05-28 14:40:22 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,20 @@
 from __future__ import print_function
 import sys
-sys.path.append("/home/yukaiwang/MTTH/models/slim")
+ 
+sys.path.append("path/to/slim")
 import tensorflow as tf
 from nets import inception_v2 as net
+
 import numpy as np 
 import os
 import imageio
 import cv2
 import time
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-is_training = True          
-
 label_set = np.genfromtxt("dataset/ucfTrainTestlist/classInd.txt", dtype ='U')
 training_set = np.genfromtxt("dataset/ucfTrainTestlist/trainlist01.txt", dtype ='U')
 testing_set = np.genfromtxt("dataset/ucfTrainTestlist/testlist01.txt", dtype = 'U')
 
-print("Train")
 data_list = training_set[:,0] 
 label_list = training_set[:,1]
 
@@ -39,7 +37,10 @@ class configuration():
         self.epoch = 0
 
     def train_batch(self):
-        x = np.zeros((self.batchsize, 224, 224, 3))
+        x1 = np.zeros((self.batchsize, 224, 224, 3))
+        x2 = np.zeros((self.batchsize, 224, 224, 3))
+        x3 = np.zeros((self.batchsize, 224, 224, 3))
+       
         y = np.zeros((self.batchsize), dtype=np.int)
         
         if len(self.query) < self.batchsize:
@@ -54,9 +55,9 @@ class configuration():
 
         for i in range(self.batchsize):
             index = pick[i]
-            x[i] = self.open_frame(index)
+            [x1[i], x2[i], x3[i]] = self.open_frame(index)
             y[i] = int(training_set[index][1]) - 1
-        return x, y
+        return x1, x2, x3, y
 
     def open_frame(self, index):
         data = training_set[index][0]
@@ -67,11 +68,16 @@ class configuration():
         h_crop = np.random.choice([256, 224, 192, 168])
         p_crop = np.random.choice(5)
 
-        sample = np.random.choice(nframes)
-        file_dir = "./frame/" + data.split('.')[0] + "/frame_%d.jpg" %sample
-        frame = imageio.imread(file_dir)
-        frame_ = self.data_augmentation(frame, flipping, w_crop, h_crop, p_crop)
-        return frame_
+        sample = [np.random.choice(int(nframes/3)), 
+                  np.random.choice(int(nframes/3)) + int(nframes/3),
+                  np.random.choice(int(nframes/3)) + 2*int(nframes/3)]
+        frames = []
+        for i in range(3):
+            file_dir = "./frame/" + data.split('.')[0] + "/frame_%d.jpg" %sample[i]
+            frame = imageio.imread(file_dir)
+            frame_ = self.data_augmentation(frame, flipping, w_crop, h_crop, p_crop)
+            frames.append(frame_)
+        return frames
 
     def data_augmentation(self, frame, flipping, l1, l2, p):
         frame = cv2.resize(frame, (340, 256), interpolation = cv2.INTER_CUBIC).astype(float)
@@ -97,12 +103,13 @@ class configuration():
         return frame
 
 conf = configuration(batchsize, training_set, testing_set)
-#batch_x1, batch_x2, batch_x3, batch_y = conf.train_batch()
 
 # define variable    
 with tf.name_scope('input_data'):
     with tf.device('/gpu:0'):                   
-        x = tf.placeholder(tf.float32, [None,  Height, Width, 3], name = 'x') 
+        x1 = tf.placeholder(tf.float32, [None,  Height, Width, 3], name = 'x1') 
+        x2 = tf.placeholder(tf.float32, [None,  Height, Width, 3], name = 'x2') 
+        x3 = tf.placeholder(tf.float32, [None,  Height, Width, 3], name = 'x3') 
     y = tf.placeholder(tf.int32, [None], name = 'y')
     y_onehot = tf.one_hot(y, n_classes, name = 'label')
     learning_rate = tf.placeholder(tf.float32, name = 'learning_rate')
@@ -111,26 +118,28 @@ slim = tf.contrib.slim
 
 with tf.device('/gpu:0'):
     with slim.arg_scope(net.inception_v2_arg_scope()) as sc:
-        _, end_points = net.inception_v2(x, num_classes = 101,
-                                           is_training=is_training, reuse=None)
-#print([v.name for v in tf.trainable_variables() if '/Logits/' in v.name])
+        _, end_points_1 = net.inception_v2(x1, num_classes = 101,
+                                           is_training=True, reuse=None)
+        _, end_points_2 = net.inception_v2(x2, num_classes = 101, 
+                                           is_training=True, reuse=True)
+        _, end_points_3 = net.inception_v2(x3, num_classes = 101,
+                                           is_training=True, reuse=True)
 variables_to_restore = slim.get_variables_to_restore(
                             include=["InceptionV2"], 
-                            #exclude=["/Logits/"])
                             exclude=[v.name for v in tf.trainable_variables() if '/Logits/' in v.name])
-print([v for v in variables_to_restore if '/Logits/' in v.name])
+
+#print([v for v in variables_to_restore if '/Logits/' in v.name])
 init_cnn = slim.assign_from_checkpoint_fn('./checkpoints/slim_models/inception_v2.ckpt',
            variables_to_restore)
 
-logits = end_points['Logits']
+logits = (end_points_1['Logits'] + end_points_2['Logits'] + end_points_3['Logits'])/3.
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_onehot, logits=logits))
 weight_decay = wd*sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
 loss = cross_entropy+weight_decay
 
 optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
-print(tf.trainable_variables())
+#print(tf.trainable_variables())
 var_train = [var for var in tf.trainable_variables() if not 'BatchNorm' in var.name]
-print(var_train)
 grads = optimizer.compute_gradients(loss, var_train)
 
 with tf.name_scope('back_propagation'):
@@ -154,12 +163,12 @@ with tf.Session() as sess:
     n_minibatches = int(128/batchsize)
     print("iter is: ", n_minibatches)
 
-    for i in range(240*n_data//128):
+    for i in range(80*n_data//128):
         start_time = time.time()
         for j in range(n_minibatches):
             start_time = time.time()
-            batch_x, batch_y = conf.train_batch()
-            feed_dict = {x:batch_x, y:batch_y, learning_rate: lr}
+            batch_x1, batch_x2, batch_x3, batch_y = conf.train_batch()
+            feed_dict = {x1:batch_x1, x2: batch_x2, x3: batch_x3, y:batch_y, learning_rate: lr}
             _, entropy, accur = sess.run([train_step, cross_entropy, accuracy], feed_dict=feed_dict)
             print('step:%4d-' %i, '{:2d}'.format(j), 
                   'loss'        , '{:8.5f}'.format(entropy), 
@@ -167,9 +176,9 @@ with tf.Session() as sess:
                   'training time: %ds' % (time.time() - start_time))
 
         if (i+1) % 500 == 0:
-            save_path = saver.save(sess, "./checkpoints/fine_tuned_cnn/inception_v2_SGD_ucfsp1/model_longest.ckpt")
+            save_path = saver.save(sess, "pass/to/model.ckpt")
             print('model saved! ', save_path)
 
-        if (i+1) % (90*n_data//128) == 0:
+        if (i+1) % (30*n_data//128) == 0:
             lr = lr/10.
             print('learing rate decreased to :', lr)
